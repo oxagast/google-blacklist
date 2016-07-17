1b086c2d1fab7ce74c395c6818376c37173ec539
Marshall Lee Whittaker <marshallwhittaker@gmail.com>
2016-07-17 16:56:36 -0400

    Made some formatting corrections, also added a loop around each character.

--- src/googet.cpp
+++ src/googet.cpp
@@ -3,36 +3,36 @@
  * Marshall Whittaker / oxagast
  */
 
-//    __ _  _  __   ___  __  ____ ____ 
+//    __ _  _  __   ___  __  ____ ____
 //   /  ( \/ )/ _\ / __)/ _\/ ___(_  _)
-//  (  O )  (/    ( (_ /    \___ \ )(  
+//  (  O )  (/    ( (_ /    \___ \ )(
 //   \__(_/\_\_/\_/\___\_/\_(____/(__)
 
-
-#include <cstdlib>
-#include <cstring>
-#include <string>
-#include <unistd.h>
-#include <stdio.h>
-#include <curl/curl.h>
-#include <iostream>
 #include <boost/algorithm/string.hpp>
-#include <boost/regex.hpp>
-#include <vector>
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
+#include <boost/regex.hpp>
+#include <cstdlib>
+#include <cstring>
+#include <curl/curl.h>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <unistd.h>
+#include <vector>
 
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
-{
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb,
+                            void *userp) {
+  ((std::string *)userp)->append((char *)contents, size * nmemb);
+  return size * nmemb;
 }
 
-std::vector<std::string> googet(std::string sub_query)
-{
+std::vector<std::string> googet(std::string sub_query) {
   // std::string sub_query = argv[1];
   // maybe needed this &tok=qrPq0XiGY77JP-GaK0ngBQ ?
-  std::string url = "https://www.google.com/complete/search?client=hp&hl=en&gs_rn=64&gs_ri=hp&gs_mss=" + sub_query + "&cp=6&gs_id=y&q=" + sub_query + "&xhr=t";
+  std::string url = "https://www.google.com/complete/"
+                    "search?client=hp&hl=en&gs_rn=64&gs_ri=hp&gs_mss=" +
+                    sub_query + "&cp=6&gs_id=y&q=" + sub_query + "&xhr=t";
   std::vector<std::string> google_suggestions;
   boost::replace_all(url, " ", "\%20");
   CURL *curl;
@@ -40,58 +40,59 @@ std::vector<std::string> googet(std::string sub_query)
   CURLcode res;
   curl_global_init(CURL_GLOBAL_DEFAULT);
   curl = curl_easy_init();
-  if(curl) {
+  if (curl) {
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-    #ifdef SKIP_PEER_VERIFICATION
+#ifdef SKIP_PEER_VERIFICATION
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-    #endif
-    #ifdef SKIP_HOSTNAME_VERIFICATION
+#endif
+#ifdef SKIP_HOSTNAME_VERIFICATION
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-    #endif
-  //  curl_easy_setopt(curl, CURLOPT_READDATA, &from_goog);
-    /* Perform request, res will return code */
-   // curl_easy_perform(curl);
+#endif
+
     /* errors */
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &read_buffer);
+    /* Perform request, res will return code */
     res = curl_easy_perform(curl);
-    if(res != CURLE_OK) {
+    if (res != CURLE_OK) {
       fprintf(stderr, "curl_easy_perform() failed: %s\n",
               curl_easy_strerror(res));
-    /* cleanup */
+      /* cleanup */
     }
 
     else {
-        /*
-         * make a bunch of formatting corrections
-         */
-        boost::replace_all(read_buffer, "\\u003cb\\u003e", "");
-        boost::replace_all(read_buffer, "\\u003c\\/b\\u003e\",0", "\n");
-        boost::replace_all(read_buffer, "[\"" + sub_query + "\",[[\"", "");
-        boost::replace_all(read_buffer, "],[\"", "");
-        boost::replace_all(read_buffer, "\",0", "\n");
-        boost::replace_all(read_buffer, "\\u003c\\/b\\u003e", "");
-        boost::replace_all(read_buffer, ",[131]", "");
-        /*
-         * create a vector for each one to sit in
-         */
-        std::vector<std::string> suggestions;
-        /*
-         * split the string up by newlines
-         */
-        boost::split(suggestions, read_buffer, boost::is_any_of("\n"), boost::token_compress_on);
-        /*
-         * get rid of that nasty last line
-         */
-        suggestions.erase(suggestions.end());
-        /*
-         * loop around the vector for each suggestion out of google
-         */
-        for (int cur_sugg = 0; cur_sugg < suggestions.size(); cur_sugg++) {
-          if (suggestions[cur_sugg].find(",[") == std::string::npos) {
-            google_suggestions.push_back(suggestions[cur_sugg]);
-          }
-        
+      /*
+       * make a bunch of formatting corrections
+       */
+      boost::replace_all(read_buffer, "\\u003cb\\u003e", "");
+      boost::replace_all(read_buffer, "\\u003c\\/b\\u003e\",0", "\n");
+      boost::replace_all(read_buffer, "[\"" + sub_query + "\",[[\"", "");
+      boost::replace_all(read_buffer, "],[\"", "");
+      boost::replace_all(read_buffer, "\",0", "\n");
+      boost::replace_all(read_buffer, "\\u003c\\/b\\u003e", "");
+      boost::replace_all(read_buffer, ",[131]", "");
+      boost::replace_all(read_buffer, "\\u0026#39;s", "'");
+      boost::replace_all(read_buffer, "\\u0026amp;", "&");
+      /*
+       * create a vector for each one to sit in
+       */
+      std::vector<std::string> suggestions;
+      /*
+       * split the string up by newlines
+       */
+      boost::split(suggestions, read_buffer, boost::is_any_of("\n"),
+                   boost::token_compress_on);
+      /*
+       * get rid of that nasty last line
+       */
+      suggestions.erase(suggestions.end());
+      /*
+       * loop around the vector for each suggestion out of google
+       */
+      for (int cur_sugg = 0; cur_sugg < suggestions.size(); cur_sugg++) {
+        if (suggestions[cur_sugg].find(",[") == std::string::npos) {
+          google_suggestions.push_back(suggestions[cur_sugg]);
+        }
       }
     }
     curl_easy_cleanup(curl);
@@ -99,4 +100,3 @@ std::vector<std::string> googet(std::string sub_query)
   curl_global_cleanup();
   return (google_suggestions);
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,50 @@
  * Marshall Whittaker / oxagast
  */
 
-//    __ _  _  __   ___  __  ____ ____ 
+//    __ _  _  __   ___  __  ____ ____
 //   /  ( \/ )/ _\ / __)/ _\/ ___(_  _)
-//  (  O )  (/    ( (_ /    \___ \ )(  
+//  (  O )  (/    ( (_ /    \___ \ )(
 //   \__(_/\_\_/\_/\___\_/\_(____/(__)
 
 // g++ src/main.cpp src/googet.cpp -o googet -lcurl --std=gnu++11
 
 #include <cstdlib>
 #include <cstring>
+#include <iostream>
+#include <stdio.h>
 #include <string>
 #include <unistd.h>
-#include <stdio.h>
-#include <iostream>
 #include <vector>
 
 std::vector<std::string> googet(std::string sub_query);
 
-int main (int argc, char *argv[]) {
-    if (argc == 2) {
+/*
+ * this routine is a wrapper for looping around the suggestions
+ * one letter at a time. it calls googet to make the https
+ * requests to google.
+ */
+std::vector<std::string> search_for(std::string query, int letters) {
+  int current_suggestion;
+  std::vector<std::string> suggestions;
+
+  suggestions = googet(query.substr(0, letters));
+  for (current_suggestion = 0; current_suggestion < suggestions.size();
+       current_suggestion++) {
+    std::cout << "Chars: " << letters << " : " << query.substr(0, letters)
+              << " : " << suggestions[current_suggestion] << std::endl;
+  }
+  return (suggestions);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 2) {
     std::string query = argv[1];
-    std::vector<std::string> suggestions = googet(query);
-    for (int current_suggestion = 0; current_suggestion < suggestions.size(); current_suggestion++) {
-        std::cout << suggestions[current_suggestion] << std::endl;
+    std::vector<std::string> sub_query_result;
+    /* loop around each letter */
+    for (int letters = 1; letters <= query.length(); letters++) {
+      sub_query_result = (search_for(query, letters));
     }
     return (0);
-    }
-    return (1);
+  }
+  return (1);
 }
